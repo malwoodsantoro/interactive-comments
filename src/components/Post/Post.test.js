cfb4c254cabf812136ff21b30dc8f13bbf19398b
mal-wood <mallorywood413@gmail.com>
2022-08-25 20:45:47 -0400

    post test

--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import {screen} from '@testing-library/dom'
+import { render, fireEvent } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
 import Post from "./Post"
 
-describe('Post', () => {
-  test('it renders comment as expected', () => {    
+describe('Comment and reply renders as expected', () => {
+  
+  test('it renders comment as expected', () => {
     const props = {
       content: 'This is some sample content',
-      username: 'juliusomo',
-      isCurrentUser: false, 
-      current: true,
+      username: 'ramsesmiron',
+      isCurrentUser: false,
+      current: 'ramsesmiron',
       score: 4
-    }   
+    }
 
     const { getByText } = render(<Post {...props} />)
-    
+
     const contentNode = getByText(props.content)
     const usernameNode = getByText(props.username)
     const replyLink = getByText('Reply')
@@ -24,21 +25,21 @@ describe('Post', () => {
     expect(replyLink).toBeDefined()
 
     const image = screen.getByAltText(/Photo of user/);
-    
+
     expect(image.src).toContain('image-');
-    expect(image).toHaveAttribute('src', 'image-juliusomo.webp')
+    expect(image).toHaveAttribute('src', 'image-ramsesmiron.webp')
   })
 
   test('it renders reply as expected', () => {
     const props = {
       content: 'This is some sample content',
       username: 'ramsesmiron',
-      isCurrentUser: false, 
+      isCurrentUser: false,
       current: true,
       replyingTo: 'maxblagun',
       score: 4,
       comment: false
-    }   
+    }
 
     const { getByText } = render(<Post {...props} />)
 
@@ -51,14 +52,33 @@ describe('Post', () => {
     expect(replyLink).toBeDefined()
 
     const image = screen.getByAltText(/Photo of user/);
-    
+
     expect(image.src).toContain('image-');
     expect(image).toHaveAttribute('src', 'image-ramsesmiron.webp')
-    
+
     const reply = props.replyingTo;
     expect(contentNode).toHaveTextContent(`${reply}`)
 
   })
+})
 
+describe('User interaction with post functions as expected', () => {
+  test('reply toggle works', () => {
+    const props = {
+      content: 'This is some sample content',
+      username: 'ramsesmiron',
+      isCurrentUser: false,
+      current: 'ramsesmiron',
+      score: 4
+    }
+
+    const { getByText } = render(<Post {...props} />)
+    const replyLink = getByText('Reply')
+
+    fireEvent.click(replyLink)
+
+    expect(screen.getByPlaceholderText(/Add a comment/)).toBeInTheDocument()
+
+  })
 
 })
