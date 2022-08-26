8219447f34341c60d2795097923387855b177e83
mal-wood <mallorywood413@gmail.com>
2022-08-26 16:25:55 -0400

    textbox tests

---
+++ src/components/Textbox/Textbox.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, fireEvent, wait } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import Textbox from './Textbos'
+
+describe('Textbox renders as expected ', () => {
+  test('the increment and decrement buttons work as expected', () => {
+  //wip
+  })
+})
